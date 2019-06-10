c51e063b0a85e115223fceecc1e0f2972b5bf533
Venkat Ram Taddi <tadivenkat@gmail.com>
2019-06-10 22:42:03 +0530

    Testing Header component snapshot using enzyme library.

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Header from '../../components/Header';
 import {shallow} from 'enzyme';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import toJSON from 'enzyme-to-json';
+//import ReactShallowRenderer from 'react-test-renderer/shallow';
 
 test('Testing Header component', () => {
     // Testcase using enzyme
     const wrapper = shallow(<Header/>);
-    expect(wrapper.find('h1').text()).toBe('Keilon Software Solutions Private Ltd');
+    //expect(wrapper.find('h1').text()).toBe('Keilon Software Solutions Private Ltd');
+    expect(toJSON(wrapper)).toMatchSnapshot();
     
     // Testcase using react-test-renderer
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header/>);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    //const renderer = new ReactShallowRenderer();
+    //renderer.render(<Header/>);
+    //expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
