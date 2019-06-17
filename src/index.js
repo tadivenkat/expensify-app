3b252213bb3f0fe67e7b1917461b8de978c55249
Venkat Ram Taddi <tadivenkat@gmail.com>
2019-06-17 19:06:28 +0530

    Updated icon

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 import AppRouter from './router/AppRouter';
-import {createStore} from 'redux';
 
 import configureStore from './store/configureStore';
 import {addExpenseAction} from './actions/expenses';
