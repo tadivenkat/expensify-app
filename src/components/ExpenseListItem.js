af99903d5035f1920db45585c2fda15994ad44bf
Venkat Ram Taddi <tadivenkat@gmail.com>
2019-06-18 03:29:55 +0530

    Formatting the currency

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = (props) => {
     const {id, amount, createdAt, description, note} = props.expense;
@@ -8,7 +9,7 @@ const ExpenseListItem = (props) => {
         <div>
             <Link to={`/edit/${id}`}><h2>{description}</h2></Link>
             <p>Id: {id}</p>
-            <p>Amount: {amount}</p>
+            <p>Amount: {numeral(amount/100).format('$0,0.00')}</p>
             <p>CreatedAt: {!!createdAt ? moment(createdAt).format('DD-MMM-YYYY') : ''}</p>
             <p>Note: {note}</p>
         </div>
